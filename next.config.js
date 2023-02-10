9612d1acec5840220c780467f82bc7a2d54b50a8
Alvin Nayak <76550424+Alve2000@users.noreply.github.com>
2023-02-11 03:12:01 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 
 /** @type {import('next').NextConfig} */
 module.exports = {
+  typescript: {
+    ignoreBuildErrors: true
+  },
   reactStrictMode: true,
   images: {
     domains: ['media.graphassets.com'],
